7b7b90ec0730b10c73252471de5f7e81a05eb8fc
ffwweeq <126655325+ffwweeq@users.noreply.github.com>
2023-06-11 22:33:26 +0800

    第三次修改

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Timer
 {
@@ -22,6 +23,7 @@ namespace Timer
     {
         List<string> hours = new List<string>();          
         List<string> minutes = new List<string>();
+        DispatcherTimer timer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
@@ -32,6 +34,16 @@ namespace Timer
 
             cmbHour.ItemsSource = hours;
             cmbMin.ItemsSource = minutes;
+
+            timer.Interval = TimeSpan.FromSeconds(1);   // 這個計時器設定每一個刻度為1秒
+            timer.Tick += new EventHandler(timer_tick); // 每一個時間刻度設定一個小程序timer_tick
+            timer.Start(); // 啟動這個計時器
+        }
+        private void timer_tick(object sender, EventArgs e)
+        {
+            txtTime.Text = DateTime.Now.ToString("HH:mm:ss");    // 顯示時間
+            txtDate.Text = DateTime.Now.ToString("yyyy-MM-dd");  // 顯示日期
+            txtWeekDay.Text = DateTime.Now.ToString("dddd");     // 顯示星期幾
         }
     }
 }
